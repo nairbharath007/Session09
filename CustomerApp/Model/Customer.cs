ed569d46308cb88387b178b6ed362608c83e5999
Bharath Nair <nairbharath007@gmail.com>
2023-08-12 19:35:06 +0530

    return customer name and customer details from array parameter

---
+++ CustomerApp/Model/Customer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerApp.Model
+{
+    internal class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int CountOrder { get; set; }
+
+        public Customer(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+        }
+
+        public Customer(int id, string name, string email, int orders) : this(id, name, email)
+        {
+            CountOrder = orders;
+        }
+
+        public string ShowCustomerDetails()
+        {
+            return $"Id: {Id}\nName: {Name}\nEmail: {Email}\nOrders: {CountOrder}";
+        }
+
+        public static string GetCustomerNameWithMaxOrders(Customer[] customers)
+        {
+            int maxOrders = customers[0].CountOrder; //10
+            string name = string.Empty;
+            for (int i = 1; i < customers.Length; i++)
+            {
+                if (customers[i].CountOrder > maxOrders)
+                {
+                    maxOrders = customers[i].CountOrder;
+                    name = customers[i].Name;
+                }
+            }
+            return name;
+        }
+        public static Customer GetCustomerDetailsWithMinOrder(Customer[] customers)
+        {
+            Customer customer = null;
+            int minOrder = customers[0].CountOrder;
+
+            for (int i = 1; i < customers.Length; i++)
+            {
+                if (customers[i].CountOrder < minOrder)
+                {
+                    minOrder = customers[i].CountOrder;
+                    customer = customers[i];
+                }
+            }
+            return customer;
+        }
+    }
+}
--- CustomerApp/Program.cs
+++ CustomerApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CustomerApp.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,32 @@ namespace CustomerApp
     {
         static void Main(string[] args)
         {
+            Customer customer1 = new Customer(101, "Name1", "name1@yahoo.com", 10);
+            Customer customer2 = new Customer(102, "Name2", "name2@gmail.com", 15);
+
+            Customer[] customers =
+            {
+                customer1,
+                customer2,
+                new Customer(103, "Name3", "name3@gmail.com"),
+                new Customer(104, "Name4", "name4@gmail.com", 25)
+            };
+
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.ShowCustomerDetails());
+                Console.WriteLine("----------------------------------------");
+            }
+
+            Console.WriteLine($"Customer with Maximum Orders: " +
+                $"{Customer.GetCustomerNameWithMaxOrders(customers)}");
+            Console.WriteLine("---------------------------------------------");
+            Customer customerObject = Customer.GetCustomerDetailsWithMinOrder(customers);
+            Console.WriteLine($"Details of Customer with Minimum Orders: " +
+                $"\n{customerObject.ShowCustomerDetails()}");
+            /*Console.WriteLine($"Details of Customer with Minimum Orders: " +
+                $"\n{Customer.GetCustomerDetailsWithMinOrder(customers).ShowCustomerDetails()}");*/
+
 
         }
     }
